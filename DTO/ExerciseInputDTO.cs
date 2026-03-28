17002bf371b636189c425fbc8e8f8ffecae91c9e
josdorrod <josediegodorado@hotmail.com>
2026-03-28 16:47:08 +0100

    feat: add ExerciseInputDTO and _exerciseForm
    Agrega DTO y partial view para ejercicios reutilizable en creación y edición.

---
+++ DTO/ExerciseInputDTO.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace GymTracker.DTO;
+
+public class ExerciseInputDTO
+{
+    /// <summary>Get or set the name of the exercise.</summary>
+    [Required]
+    public string Name { get; set; } = String.Empty;
+    /// <summary>Get or set the muscle group targeted by the exercise.</summary>
+    [MaxLength(50)]
+    public string? MuscleGroup { get; set; }
+    /// <summary>Get or set the number of planned sets for the exercise.</summary>
+    [Range(1, 10, ErrorMessage = "El número de series debe ser mayor a 0 y menor a 10.")]
+    public int PlannedSets { get; set; } = 3;
+    /// <summary>Get or set the instructions for the exercise.</summary>
+    public string? Instructions { get; set; }
+}
--- Pages/Exercises/Create.cshtml.cs
+++ Pages/Exercises/Create.cshtml.cs
@@ -1,14 +1,16 @@
 using GymTracker.Data;
 using GymTracker.Data.Entities;
+using GymTracker.DTO;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Validation;
 
 namespace GymTracker.Pages.Exercises;
 
 public class CreateModel(GymTrackerDbContext db) : PageModel
 {
     [BindProperty]
-    public Exercise Input { get; set; } = new();
+    public ExerciseInputDTO Input { get; set; } = new();
 
     public void OnGet() { }
 
@@ -16,10 +18,18 @@ public class CreateModel(GymTrackerDbContext db) : PageModel
     {
         if (!ModelState.IsValid)
             return Page();
+        
+        var exercise = new Exercise
+        {
+            Name = Input.Name,
+            MuscleGroup = Input.MuscleGroup,
+            PlannedSets = Input.PlannedSets,
+            Instructions = Input.Instructions
+        };
 
-        db.Exercises.Add(Input);
+        db.Exercises.Add(exercise);
         await db.SaveChangesAsync();
 
-        return RedirectToPage("Detail", new { id = Input.Id });
+        return RedirectToPage("Detail", new { id = exercise.Id });
     }
 }
