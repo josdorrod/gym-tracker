ccfe439148dbfce217a92de3cdd64509ab510436
josdorrod <josediegodorado@hotmail.com>
2026-03-28 11:51:52 +0100

    test: test remove sets

--- GymTracker.Tests/Exercises/DetailModelTests.cs
+++ GymTracker.Tests/Exercises/DetailModelTests.cs
@@ -54,4 +54,74 @@ public class DetailModelTests
         // Assert
         result.Should().BeOfType<NotFoundResult>();
     }
+    [TestMethod]
+    public async Task OnPostDeleteAsync_DeletesExistingSet_Success()
+    {
+        // Arrange
+        using var db = CreateDb();
+        var exercise = new Exercise { Name = "Sentadilla", MuscleGroup = "Pierna" };
+        db.Exercises.Add(exercise);
+        var set = new Set { Exercise = exercise, Weight = 100, Reps = 5, CreatedAtUtcTicks = DateTimeOffset.UtcNow.UtcTicks };
+        db.Sets.Add(set);
+        await db.SaveChangesAsync();
+        var model = new DetailModel(db);
+
+        // Act
+        var result = await model.OnPostDeleteAsync(set.Id);
+
+        // Assert
+        result.Should().BeOfType<RedirectToPageResult>();
+        db.Sets.Where(s => s.ExerciseId == exercise.Id).Should().BeEmpty();
+        db.Exercises.Find(exercise.Id).Should().NotBeNull();
+    }
+
+    [TestMethod]
+    public async Task OnPostDeleteAsync_NonExistentSetId_ReturnsNotFound()
+    {
+        // Arrange
+        using var db = CreateDb();
+        var exercise = new Exercise { Name = "Press Militar", MuscleGroup = "Hombro" };
+        db.Exercises.Add(exercise);
+        var set = new Set { Exercise = exercise, Weight = 50, Reps = 10, CreatedAtUtcTicks = DateTimeOffset.UtcNow.UtcTicks };
+        db.Sets.Add(set);
+        await db.SaveChangesAsync();
+        var model = new DetailModel(db);
+
+        // Act
+        var result = await model.OnPostDeleteAsync(set.Id + 999); // Non-existent setId
+
+        // Assert
+        result.Should().BeOfType<NotFoundResult>();
+        db.Sets.Find(set.Id).Should().NotBeNull();
+        db.Exercises.Find(exercise.Id).Should().NotBeNull();
+    }
+
+    [TestMethod]
+    public async Task OnPostDeleteAsync_MultipleDeletions_OnlyTargetedSetRemoved()
+    {
+        // Arrange
+        using var db = CreateDb();
+        var exercise = new Exercise { Name = "Remo", MuscleGroup = "Espalda" };
+        db.Exercises.Add(exercise);
+        var set1 = new Set { Exercise = exercise, Weight = 60, Reps = 12, CreatedAtUtcTicks = DateTimeOffset.UtcNow.UtcTicks };
+        var set2 = new Set { Exercise = exercise, Weight = 65, Reps = 10, CreatedAtUtcTicks = DateTimeOffset.UtcNow.UtcTicks + 1 };
+        var set3 = new Set { Exercise = exercise, Weight = 70, Reps = 8, CreatedAtUtcTicks = DateTimeOffset.UtcNow.UtcTicks + 2 };
+        db.Sets.AddRange(set1, set2, set3);
+        await db.SaveChangesAsync();
+        var model = new DetailModel(db);
+
+        // Act & Assert
+        (await model.OnPostDeleteAsync(set1.Id)).Should().BeOfType<RedirectToPageResult>();
+        db.Sets.Where(s => s.ExerciseId == exercise.Id).Should().HaveCount(2);
+        db.Sets.Find(set2.Id).Should().NotBeNull();
+        db.Sets.Find(set3.Id).Should().NotBeNull();
+
+        (await model.OnPostDeleteAsync(set2.Id)).Should().BeOfType<RedirectToPageResult>();
+        db.Sets.Where(s => s.ExerciseId == exercise.Id).Should().HaveCount(1);
+        db.Sets.Find(set3.Id).Should().NotBeNull();
+
+        (await model.OnPostDeleteAsync(set3.Id)).Should().BeOfType<RedirectToPageResult>();
+        db.Sets.Where(s => s.ExerciseId == exercise.Id).Should().BeEmpty();
+        db.Exercises.Find(exercise.Id).Should().NotBeNull();
+    }
 }
